83ff26f3c469abf68d7dfba51b27639aa755f4cd
CBrandess <carlybrandess@Carly-Brandesss-MacBook-Pro.local>
2016-01-30 22:00:05 -0600

    Final Project First Draft
    Still have to do CSS and have some work do in figuring out exactly how
    to make the photography page and contact link work.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// When hompage loads full screen first background image will begin to cycle between several images.
+
+// When user clicks on about me link in nav bar the corresponding page loads.
+
+// When user clicks on photography link in nav bar the corresponding page loads.
+
+// When user clicks on image positioned over title (ex: Ireland, Iceland, Israel, etc.) the image enlarges and opens as first image in image carousel. 
+	 // clear any image before loading next image
+	 // When user clicks on right arrow on image the next image loads. This can cycle back through the deck multiple times. 
+	 // When user clicks on left arrow on image the previous image loads. This can cyle through the deck multiple times. 
+
+
+// When user clicks on contact in nav bar a form pops up.
+	// user enters name, email address, and message
+	// when user pushes send button the email is sent and form is cleared from the window. 
