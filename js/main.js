d95885c1feb9f3ba72ffb2fe53888d0c81d3ea42
CBrandess <carlybrandess@Carly-Brandesss-MacBook-Pro.local>
2016-02-13 23:43:55 -0600

    Updates

--- js/main.js
+++ js/main.js
@@ -1,15 +1,30 @@
-// When hompage loads full screen first background image will begin to cycle between several images.
-
 // When user clicks on about me link in nav bar the corresponding page loads.
 
 // When user clicks on photography link in nav bar the corresponding page loads.
-
-// When user clicks on image positioned over title (ex: Ireland, Iceland, Israel, etc.) the image enlarges and opens as first image in image carousel. 
-	 // clear any image before loading next image
-	 // When user clicks on right arrow on image the next image loads. This can cycle back through the deck multiple times. 
-	 // When user clicks on left arrow on image the previous image loads. This can cyle through the deck multiple times. 
 
 
 // When user clicks on contact in nav bar a form pops up.
 	// user enters name, email address, and message
 	// when user pushes send button the email is sent and form is cleared from the window. 
+
+
+function cycleBackgrounds() {
+	var index = 0;
+
+	$imageEls = $('.toggle-image'); // Get the images to be cycled.
+
+	setInterval(function () {
+		$imageEls.eq(index).removeClass('show');
+		// Get the next index.  If at end, restart to the beginning.
+		index = index + 1 < $imageEls.length ? index + 1 : 0;
+		// Show the next image.
+		$imageEls.eq(index).addClass('show');
+		// Hide the previous image.
+
+	}, 3000);
+};
+
+// Document Ready.
+$(function () {
+	cycleBackgrounds();
+});
