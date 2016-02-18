068804938c1d369d73e216d3f666885605a230ae
CBrandess <carlybrandess@Carly-Brandesss-MacBook-Pro.local>
2016-02-18 00:11:46 -0600

    Updates

--- js/main.js
+++ js/main.js
@@ -6,6 +6,24 @@ $('.getintouch').on('click', function() {
 	$('.dropdown-menu').slideToggle();
 });
 
+(function() {
+
+  // On icon click...
+  $('.menu-link-wrapper').on('click.mobileNav', function(){
+
+    // Toggle the open/closed state of the menu icon
+    $('.menu-link-wrapper .menu-link').toggleClass('menu-trigger-open');
+
+  });
+  
+})();
+
+$('.menu-link-wrapper').on('click', function(){
+  $('.mainNavDropDown').slideToggle(500);
+ });
+
+
+
 function cycleBackgrounds() {
 	var index = 0;
 
@@ -29,3 +47,5 @@ function cycleBackgrounds() {
 $(function () {
 	cycleBackgrounds();
 });
+
+
